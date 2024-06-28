b9597db542cc4ddbeda23d52ca6d39be8de2081c
Reetika <140263337+Reetika78@users.noreply.github.com>
2024-06-28 15:47:49 +0530

    Add files via upload

---
+++ service.js
@@ -0,0 +1,19 @@
+
+const QRCode = require('qrcode');
+
+exports.formatData = (data) => {
+    const qrCodeText = `Product ID: ${data.id}, Price: ₹${data.price}`;
+    return qrCodeText;
+};
+
+exports.generateQRCode = async (qrCodeText) => {
+    const options = {
+        errorCorrectionLevel: 'M',
+        type: 'image/png',
+        margin: 1
+    };
+
+    const qrCodeBuffer = await QRCode.toBuffer(qrCodeText, options);
+    return qrCodeBuffer;
+};
+
